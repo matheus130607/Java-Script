8571579f40ac5ba339ad993908eaf1b773f69ed6
Matheus <matheusmalaman2014@gmail.com>
2025-05-15 15:49:32 -0300

    Pizzaria

--- Projeto Pizzaria/Projeto Principal/pizzaria.js
+++ Projeto Pizzaria/Projeto Principal/pizzaria.js
@@ -12,13 +12,13 @@ function mostrarPizza(secao) {
 }
 
 function adicionarPizza() {
-    const pizza = document.getElementById("pizza").value;
+    const sabor = document.getElementById("sabor").value;
     const ingredientes = document.getElementById("ingredientes").value;
     const preco = parseFloat(document.getElementById("preco").value);
 
-    if(pizza && ingredientes && preco) {
-        pizzaria.push({pizza, ingredientes, preco});
-        document.getElementById("pizza").value = "";
+    if(sabor && ingredientes && preco) {
+        pizzaria.push({sabor, ingredientes, preco});
+        document.getElementById("sabor").value = "";
         document.getElementById("ingredientes").value = "";
         document.getElementById("preco").value = "";
         atualizarLista();
@@ -30,19 +30,19 @@ function adicionarPizza() {
 
 function buscarPizza() {
     const busca = document.getElementById("busca").value.toLowerCase();
-    const resultados = pizzaria.filter((pizzas) =>
-    pizzas.pizza.toLowerCase().includes(busca));
+    const resultados = pizzaria.filter((pizza) =>
+    pizza.sabor.toLowerCase().includes(busca));
     atualizarLista(resultados);
 }
 
 function buscarPizzaParaAlterar() {
     const busca = document.getElementById("busca-alterar").value.toLowerCase();
-    pizzaParaAlterar = pizzaria.find((pizzas) =>
-    pizzas.pizza.toLowerCase().includes(busca));
+    pizzaParaAlterar = pizzaria.find((pizza) =>
+    pizza.sabor.toLowerCase().includes(busca));
 
     if (pizzaParaAlterar) {
         document.getElementById("form-alterar").classList.remove("hidden");
-        document.getElementById("nova-pizza").value = pizzaParaAlterar.pizza;
+        document.getElementById("novo-sabor").value = pizzaParaAlterar.sabor;
         document.getElementById("novo-ingredientes").value = pizzaParaAlterar.ingredientes;
         document.getElementById("novo-preco").value = pizzaParaAlterar.preco;
     } else {
@@ -52,9 +52,101 @@ function buscarPizzaParaAlterar() {
 
 function alterarPizza() {
     if (pizzaParaAlterar) {
-        const novaPizza = document.getElementById
-        ("nova-pizza").value;
-        const novoIngredientes = document.getElementById
+        const novoSabor = document.getElementById
+        ("novo-sabor").value;
+        const novoIngredientes = document.getElementById ("novo-ingredientes").value;
+        const novoPreco = parseFloat(document.getElementById("novo-preco").value);
+
+        if(novoSabor && novoIngredientes && novoPreco) {
+            pizzaParaAlterar.sabor = novoSabor;
+            pizzaParaAlterar.ingredientes = novoIngredientes;
+            pizzaParaAlterar.preco = novoPreco;
+
+            atualizarLista();
+            alert("Pizza alterado com sucesso!");
+            document.getElementById("form-alterar").classList.add("hidden");
+        } else {
+            alert("Por favor, preencha todos os campos");
+        }
     }
+}
+
+function atualizarLista(lista = pizzaria) {
+    const tabela = document.getElementById("lista-pizzas");
+    tabela.innerHTML = "";
+
+    lista.forEach((pizza) => {
+        const linha = document.createElement("tr");
+        linha.innerHTML = `
+        <td>${pizza.sabor}</td>
+        <td>${pizza.ingredientes}</td>
+        <td>${pizza.preco}</td>
+        `;
+        tabela.appendChild(linha);
+    });
+}
+
+let vendas = [];
+
+function registrarVenda() {
+    const sabor = document.getElementById('venda-sabor').value;
+    const preco = document.getElementById('venda-preco').value;
+    const comprador = document.getElementById('venda-comprador').value;
+
+    if (sabor && preco && comprador) {
+     const listaVendas = document.getElementById('lista-vendas');
+     const item = document.createElement('li');
+     item.textContent = ` Sabor: ${sabor}, Preço: R$${preco}, Comprador: ${comprador}`;
+     listaVendas.appendChild(item);   
+
+     vendas.push({sabor, preco, comprador});
+
+     document.getElementById('venda-sabor').value = '';
+     document.getElementById('venda-preco').value = '';
+     document.getElementById('venda-comprador').value = '';
+
+    } else {
+        alert('Por favor, preencha todos os campos!');
+    }
+}
+
+function gerarRelatorioVendas() {
+    const tabelaRelatorio = document.getElementById('tabela-relatorio-vendas');
+    tabelaRelatorio.innerHTML = '';
+
+    if (vendas.length === 0) {
+        alert('Nenhuma venda registrada');
+        return;
+    }
+
+    let totalVendas = 0;
+    
+    if(totalVendas.length === 0) {
+        alert('Valor de venda não registrada')
+        return;
+    }
+
+    vendas.forEach((venda) => {
+        const linha = document.createElement('tr');
+        linha.innerHTML = `
+        <td> ${venda.sabor}</td>
+        <td>R$${parseFloat(venda.preco).toFixed(2)}</td>
+        <td>${venda.comprador}</td>
+        `;
+        tabelaRelatorio.appendChild(linha);
+
+        totalVendas += parseFloat(venda.preco);
+    });
+
+    const linhaTotal = document.createElement('tr');
+    linhaTotal.innerHTML = `
+    <td><strong>Total</strong></td>
+    <td><strong>R$${totalVendas.toFixed(2)}</strong></td>
+    <td></td>
+    `;
+    tabelaRelatorio.appendChild(linhaTotal);
+
+    document.getElementById('relatorio-vendas').
+    classList.remove('hidden');
 }
 
